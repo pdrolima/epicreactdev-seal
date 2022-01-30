4b28014bc08f712f356b2f5d66eace737e4ba8e3
webmasterdro <pdro.lima@pm.me>
2022-01-29 22:08:12 -0300

    Exercise 02: 💯 effect dependencies

---
+++ react-hooks/src/exercise/02.extra-2.js
@@ -0,0 +1,37 @@
+// useEffect: persistent state
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+import {useEffect} from "react";
+
+function Greeting({initialName = ''}) {
+  // 🐨 initialize the state to the value from localStorage
+  // 💰 window.localStorage.getItem('name') ?? initialName
+  const [name, setName] = React.useState(() => window.localStorage.getItem('name') ?? initialName)
+
+  // 🐨 Here's where you'll use `React.useEffect`.
+  // The callback should set the `name` in localStorage.
+  // 💰 window.localStorage.setItem('name', name)
+  useEffect(() => {
+      window.localStorage.setItem('name', name)
+  }, [name])
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
