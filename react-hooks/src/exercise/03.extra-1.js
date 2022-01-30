1cc31dab771324d69178ea18b3144d055ebfd243
webmasterdro <pdro.lima@pm.me>
2022-01-30 18:25:14 -0300

    Exercise 03: 💯 colocating state

---
+++ react-hooks/src/exercise/03.extra-1.js
@@ -0,0 +1,49 @@
+// Lifting state
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+
+function Name() {
+   const [name, setName] = React.useState('')
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input id="name" value={name} onChange={event => setName(event.target.value)} />
+    </div>
+  )
+}
+
+// 🐨 accept `animal` and `onAnimalChange` props to this component
+function FavoriteAnimal({ animal, onAnimalChange }) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input
+        id="animal"
+        value={animal}
+        onChange={onAnimalChange}
+      />
+    </div>
+  )
+}
+
+// 🐨 uncomment this
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
+}
+
+function App() {
+  // 🐨 add a useState for the animal
+  const [animal, setAnimal] = React.useState('')
+  return (
+    <form>
+      <Name  />
+      {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
+      <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)} />
+      {/* 🐨 pass the animal prop here */}
+      <Display animal={animal} />
+    </form>
+  )
+}
+
+export default App
