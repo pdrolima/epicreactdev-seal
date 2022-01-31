4082e1d878464fd9ca3e1b0b51d12249c0ea2be0
webmasterdro <pdro.lima@pm.me>
2022-01-30 23:42:09 -0300

    Exercise 05

--- react-hooks/src/exercise/05.js
+++ react-hooks/src/exercise/05.js
@@ -4,32 +4,25 @@
 import * as React from 'react'
 // eslint-disable-next-line no-unused-vars
 import VanillaTilt from 'vanilla-tilt'
+import {useEffect} from "react";
 
 function Tilt({children}) {
-  // 🐨 create a ref here with React.useRef()
+    const tiltRootRef = React.useRef();
+    useEffect(() => {
+        const tiltNode = tiltRootRef.current;
+        VanillaTilt.init(tiltNode, {
+            max: 25,
+            speed: 400,
+            glare: true,
+            'max-glare': 0.5,
+        });
+        return () => tiltNode.vanillaTilt.destroy();
+    }, []);
 
-  // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
-  // div look fancy.
-  // 💰 like this:
-  // const tiltNode = tiltRef.current
-  // VanillaTilt.init(tiltNode, {
-  //   max: 25,
-  //   speed: 400,
-  //   glare: true,
-  //   'max-glare': 0.5,
-  // })
-  //
-  // 💰 Don't forget to return a cleanup function. VanillaTilt.init will add an
-  // object to your DOM node to cleanup:
-  // `return () => tiltNode.vanillaTilt.destroy()`
-  //
-  // 💰 Don't forget to specify your effect's dependencies array! In our case
-  // we know that the tilt node will never change, so make it `[]`. Ask me about
-  // this for a more in depth explanation.
 
-  // 🐨 add the `ref` prop to the `tilt-root` div here:
+    // 🐨 add the `ref` prop to the `tilt-root` div here:
   return (
-    <div className="tilt-root">
+    <div className="tilt-root" ref={tiltRootRef}>
       <div className="tilt-child">{children}</div>
     </div>
   )
