a1fd16aecc80cbee7a617fbc9bf4c7839ae468cd
webmasterdro <pdro.lima@pm.me>
2022-02-02 11:23:05 -0300

    Exercise 06

--- react-hooks/src/exercise/06.js
+++ react-hooks/src/exercise/06.js
@@ -6,26 +6,42 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {fetchPokemon, PokemonDataView, PokemonForm, PokemonInfoFallback} from '../pokemon'
+import {useEffect} from "react";
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [pokemon, setPokemon] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
-  // 💣 remove this
-  return 'TODO'
+  useEffect(() => {
+      if (!pokemonName) {
+          return;
+      }
+
+      setPokemon(null);
+      setError(null);
+      fetchPokemon(pokemonName).then(pokemon =>
+          setPokemon(pokemon),
+          error => setError(error)
+      );
+
+  }, [pokemonName]);
+
+
+   if(error) {
+      return (
+          <div role="alert">
+              There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+          </div>
+      )
+   } else    if (!pokemonName) {
+        return 'Submit a pokemon'
+    } else if (!pokemon) {
+        return <PokemonInfoFallback name={pokemonName} />
+    } else {
+        return <PokemonDataView pokemon={pokemon} />
+    }
 }
 
 function App() {
